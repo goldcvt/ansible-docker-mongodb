9c94f3e6bf6d8039a0e36cccc4eb946fde0dad45
goldcvt <thegoldcat5@gmail.com>
2021-03-17 12:33:34 +0300

    Delete terraform_mongo directory

--- terraform_mongo/secret.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_access_key" {
-  description = "AWS access key."
-  default	= "SomeRandomAccessKey"
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key."
-  default     = "SomeRandomSecretKey"
-}
